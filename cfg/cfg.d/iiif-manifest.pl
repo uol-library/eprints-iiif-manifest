a694516af002a1c88fd12b4b073d220b613ba90b
peteredwards <p.l.edwards@leeds.ac.uk>
2021-11-17 16:07:28 +0000

    Added on_generate_thumbnails and changed location of manifests cache

--- cfg/cfg.d/iiif-manifest.pl
+++ cfg/cfg.d/iiif-manifest.pl
@@ -1,1 +1,13 @@
 $c->{plugins}{'Export::IIIFManifest'}{params}{disable} = 0;
+$c->{on_generate_thumbnails} = sub
+{
+    my ($session, $doc) = @_;
+    my $eprint = $doc->parent;
+    my $dir  = $session->get_repository->config( 'variables_path' ) . '/iiif-manifests';
+    my $id   = $eprint->value( 'eprintid' );
+    my $file = "$dir/$id.json";
+    if ( -e $file )
+    {
+        unlink $file;
+    }
+}
